616c6cdcce5967e2558196392e99267a053d78c9
Morgan <mrh9724@lanaddict.com>
2011-06-27 16:41:50 -0400

    Using Creative Commons  Attribution-NonCommercial-ShareAlike 3.0 License

--- src/net/milkbowl/autosave/customvault/AutoSaveVaultPermission.java
+++ src/net/milkbowl/autosave/customvault/AutoSaveVaultPermission.java
@@ -1,19 +1,13 @@
 /**
+ * 
  * Copyright 2011 Morgan Humes
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of
+ * this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send
+ * a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ * 
  */
 
 package net.milkbowl.autosave.customvault;
